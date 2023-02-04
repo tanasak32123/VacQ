5ef85cc050e51588c9f02bedc605309dca097d91
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-02-04 20:37:00 +0700

    REST+MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
-//Route files
-const hospitals = require("./routes/hospitals");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//Route files
+const hospitals = require("./routes/hospitals");
+
+//Connect to database
+connectDB();
+
 const app = express();
+
+//Body parser
+app.use(express.json());
 
 //Mount routers
 app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
   console.log(
     "Server is running in ",
@@ -22,3 +30,10 @@ app.listen(
     PORT
   )
 );
+
+//Handle unhandled promise rejection
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
