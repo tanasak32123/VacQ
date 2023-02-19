91804afbbd4b113aeff2e40f228f37dfe5c88a5b
Tanasak Pusawatwong <tanasak32123@gmail.com>
2023-02-19 09:25:37 +0700

    Authentication

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 //load env vars
@@ -7,6 +8,8 @@ dotenv.config({ path: "./config/config.env" });
 
 //Route files
 const hospitals = require("./routes/hospitals");
+
+const auth = require("./routes/auth");
 
 //Connect to database
 connectDB();
@@ -16,8 +19,12 @@ const app = express();
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
 //Mount routers
 app.use("/api/v1/hospitals", hospitals);
+app.use("/api/v1/auth", auth);
 
 const PORT = process.env.PORT || 5000;
 
